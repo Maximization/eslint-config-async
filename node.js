5a975e48d0cdfa24af36fa14e9018b4dd3085ec5
Ben Szabo <10125940+b3nk3@users.noreply.github.com>
2024-08-26 21:29:48 +0100

    Update ESLint to v9, and typescript-eslint to v8
    * chore: bump peer dependency version
    
    * Update README.md
    
    * Upgrade to ESLint v9
    
    - Update supported Node.js versions to conform to ESLint v9
    - Replace eslint-config-node (unmaintaned) with eslint-config-n. Also move from peer dependency to dependency
    - Replace @typescript-eslint/parser & @typescript-eslint/eslint-plugin with typescript-eslint
    - Mark typescript as optional peer dependency
    - Merge index.js, node.js, and typescript.js into single file with named exports
    - Update Installation instructions for Non-TS and TS users
    - Add migration guide from v2 to v3
    - Add ESLint & Node.js version requirements, and direct ESLint v8 users to v2
    
    * Update package-lock.json
    
    ---------
    
    Co-authored-by: Maxim Orlov <maximorl@gmail.com>

--- node.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  rules: {
-    // Node.js rules
-    "node/handle-callback-err": ["error", "^(e|err|error)$"],
-    "node/no-callback-literal": "error",
-    "node/no-sync": "error",
-  },
-};
--- typescript.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rules: {
-    // TypeScript rules
-    "@typescript-eslint/await-thenable": "error",
-    "@typescript-eslint/no-floating-promises": "error",
-    "@typescript-eslint/no-misused-promises": "error",
-    "@typescript-eslint/promise-function-async": "error",
-  },
-};
